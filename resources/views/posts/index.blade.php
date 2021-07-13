5f698a35810857cf68ac626b8c5d786e30fb7e52
EC2 Default User <ec2-user@ip-172-31-15-191.ap-southeast-1.compute.internal>
2021-07-13 10:21:13 +0000

    add Task_1～Task_4

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts')
+
+@section('content')
+  
+  
+    <div class="container mt-4">
+        @foreach ($posts as $post)
+        <div class="card mt-4">
+            <div class="card-header mb-2">
+                {{ $post->title }}
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    {{ $post->body }}
+                </p>
+                
+                
+            </div>
+            <div class="card-footer">
+                <span class="mr-2">
+                    投 稿 日 時
+                </span>
+                
+            </div>
+        </div>
+        @endforeach
+    </div>
+    
+@endsection('content')
